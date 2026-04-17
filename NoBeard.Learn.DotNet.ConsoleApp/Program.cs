30c032dd99fa3923d6ef95cedf2e9c51e6b32f9d
Ratko Ćosić <ratko_cosic@hotmail.com>
2026-04-17 10:02:09 +0200

    Add project files.

---
+++ NoBeard.Learn.DotNet.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NoBeard.Learn.DotNet.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
