021f89bd0369a013f097b9855e8322e597930546
Ratko Ćosić <ratko_cosic@hotmail.com>
2026-05-07 20:13:46 +0200

    in

--- NoBeard.Learn.DotNet.ConsoleApp/Program.cs
+++ NoBeard.Learn.DotNet.ConsoleApp/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace NoBeard.Learn.DotNet.ConsoleApp
+﻿int broj = 10;
+
+//Nullable<int> slobodni = null;
+int? slobodni = null;
+
+broj = slobodni ?? 0;
+
+slobodni += 2;
+
+broj = slobodni.GetValueOrDefault();
+
+if (!slobodni.HasValue)
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+    Console.Write("to je null");
 }
+
+Console.ReadLine();
+
+
+
