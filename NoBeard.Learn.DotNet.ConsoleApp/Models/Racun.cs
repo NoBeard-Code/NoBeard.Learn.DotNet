53268c9181c797578705b2a7e9428edb00f76f12
Ratko Ćosić <ratko_cosic@hotmail.com>
2026-05-07 17:50:44 +0200

    init

---
+++ NoBeard.Learn.DotNet.ConsoleApp/Models/Racun.cs
@@ -0,0 +1,39 @@
+﻿namespace NoBeard.Learn.DotNet.ConsoleApp.Models;
+
+internal class Racun 
+{
+    //public delegate void EventHandler(object? sender, EventArgs e);
+
+    public event EventHandler<double> StanjePromjenjeno;
+
+    private double _stanje;
+
+    public double Stanje
+    {
+        get
+        {
+            return _stanje;
+        }
+        private set
+        {
+            _stanje = value;
+            StanjePromjenjeno?.Invoke(this, value);
+        }
+    }
+
+    public void Uplati(double iznos)
+    {
+        Stanje += iznos;
+        //StanjePromjenjeno.Invoke(this, Stanje);
+    }
+
+    public void Isplati(double iznos)
+    {
+        Stanje -= iznos;
+    }
+
+    public void IsplatiSve()
+    {
+        Stanje = 0;
+    }
+}
--- NoBeard.Learn.DotNet.ConsoleApp/Program.cs
+++ NoBeard.Learn.DotNet.ConsoleApp/Program.cs
@@ -1,10 +1,48 @@
-﻿namespace NoBeard.Learn.DotNet.ConsoleApp
+﻿using NoBeard.Learn.DotNet.ConsoleApp.Models;
+
+namespace NoBeard.Learn.DotNet.ConsoleApp
 {
-    internal class Program
+    internal class Program : IDisposable
     {
+        private static Racun racun = new Racun();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //var racun = new Racun();
+
+            racun.StanjePromjenjeno += Racun_StanjePromjenjeno;
+            //racun.StanjePromjenjeno += Racun_StanjePromjenjeno;
+
+            racun.Uplati(50.00);
+            //Console.WriteLine("Stanje je: " + racun.Stanje);
+
+            racun.Uplati(120.00);
+            //Console.WriteLine("Stanje je: " + racun.Stanje);
+
+            racun.Isplati(20.00);
+            //Console.WriteLine("Stanje je: " + racun.Stanje);
+
+            racun.IsplatiSve();
+            //Console.WriteLine("Stanje je: " + racun.Stanje);
+
+            racun.StanjePromjenjeno -= Racun_StanjePromjenjeno;
+
+            //racun = null;
+
+            //racun.Uplati(500000.00);
+        }
+
+        private static void Racun_StanjePromjenjeno(object? sender, double e)
+        {
+            Console.WriteLine("Stanje je: " + e);
+        }
+
+        public void Dispose()
+        {
+            if (racun != null)
+            {
+                racun.StanjePromjenjeno -= Racun_StanjePromjenjeno;
+            }
         }
     }
 }
