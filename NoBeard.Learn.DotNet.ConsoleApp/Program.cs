cdff769a2e5bc79acf5f097e5e1fba03805b7e9b
Ratko Ćosić <ratko_cosic@hotmail.com>
2026-05-11 18:26:54 +0200

    ext

--- NoBeard.Learn.DotNet.ConsoleApp/Program.cs
+++ NoBeard.Learn.DotNet.ConsoleApp/Program.cs
@@ -3,18 +3,48 @@
 //Nullable<int> slobodni = null;
 int? slobodni = null;
 
-broj = slobodni ?? 0;
+broj = slobodni.HasValue ? slobodni.Value : 0; // ternary conditional operator
+
+broj = slobodni ?? 0; // null-coalescing operator
 
 slobodni += 2;
 
 broj = slobodni.GetValueOrDefault();
 
-if (!slobodni.HasValue)
+if (slobodni.HasValue)
+{
+    Console.Write(slobodni.Value);
+}
+else
 {
     Console.Write("to je null");
 }
 
+Osoba? osoba = null; // = new Osoba();
+
+if (osoba != null && osoba.Sifra > 10)
+{
+
+}
+
+if (osoba?.Sifra > 10)
+{
+
+}
+
+osoba = new Osoba();
+
+if (osoba!.Sifra > 10)
+{
+
+}
+
 Console.ReadLine();
 
+public class Osoba
+{
+    public int? Sifra { get; set; }
 
+    public string Naziv { get; set; }
+}
 
