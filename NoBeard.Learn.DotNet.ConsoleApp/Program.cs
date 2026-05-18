f8db7227e30d1906b3ca7119a2b10974c0142dd5
Ratko Ćosić <ratko_cosic@hotmail.com>
2026-05-18 19:53:37 +0200

    add

--- NoBeard.Learn.DotNet.ConsoleApp/Program.cs
+++ NoBeard.Learn.DotNet.ConsoleApp/Program.cs
@@ -1,43 +1,130 @@
-﻿
-var ime = "Pero";
-var id = 10;
+﻿namespace NoBeard.Learn.DotNet.ConsoleApp;
 
-var polaznik = new { Id = 10, Ime = "Pero" };
-
-Console.WriteLine($"Sifra: {polaznik.Id}, Ime: {polaznik.Ime}");
-
-// polaznik.Id = 20;
-
-var osoba = new
+internal class Program
 {
-    Sifra = 100,
-    Ime = "Pero",
-    Prezime = "Perić",
-    GodinaRodjenja = 2026,
-    DatumUpisa = DateTime.Today,
-    Racun = new
+    public delegate void HelloWorldDelegate(string message);
+
+    static void Main(string[] args)
     {
-        Sifra = 1000,
-        Naziv = "Superbanka",
-        Stanje = 100.00F
+        #region Anonimni tipovi
+
+        string ime = "Pero";
+        var id = 10;
+
+        var polaznik = new { Id = 10, Ime = "Pero" };
+
+        Console.WriteLine($"Sifra: {polaznik.Id}, Ime: {polaznik.Ime}");
+
+        var polaznik2 = new { id, ime };
+
+        // polaznik.Id = 20;
+
+        var osoba = new
+        {
+            Sifra = 100,
+            Ime = "Pero",
+            Prezime = "Perić",
+            GodinaRodjenja = 2026,
+            DatumUpisa = DateTime.Today,
+            Racun = new
+            {
+                Sifra = 1000,
+                Naziv = "Superbanka",
+                Stanje = 100.00F
+            }
+        };
+
+        Console.WriteLine($"Osoba, Ime: {osoba.Ime}, Račun, Naziv: {osoba.Racun.Naziv}");
+
+        var polaznici = new[]
+        {
+            new { Id = 10, Name = "Pero" },
+            new { Id = 20, Name = "Anja" },
+            new { Id = 30, Name = "Maja" },
+            new { Id = 40, Name = "Marko" }
+        };
+
+        var zadnji = polaznici.Last();
+        var pretraga = polaznici.Where(_ => _.Id > 10).ToList();
+
+        pretraga.ForEach(x => Console.WriteLine($"Polaznik: šifra: {x.Id}, ime: {x.Name}"));
+
+        #endregion
+
+        #region Anonimne metode
+
+        //HelloWorldDelegate delegat = new HelloWorldDelegate(PozivnaMetoda);
+        //HelloWorldDelegate delegat = PozivnaMetoda;
+
+        //void PozivnaMetoda(string message)
+        //{
+        //    Console.WriteLine($"{message}");    
+        //}
+
+        HelloWorldDelegate delegat = delegate (string message)
+        {
+            Console.WriteLine($"Hello world, {message}!");
+        };
+
+        //delegat.Invoke("Pero");
+        delegat("Pero");
+
+        ObaviRadnju(delegat);
+
+        var metoda = (string message) =>
+        {
+            Console.WriteLine($"Hello world from {ime}: {message}!");
+        };
+
+        var osobe = new List<Osoba>()
+        { 
+            new Osoba(1, "Pero"),
+            new Osoba(2, "Maja"),
+        };
+
+        osobe.ForEach(new Action<Osoba>(IzlistajOsobu));
+        osobe.ForEach(clan => Console.WriteLine($"Hello world from {clan.Naziv}!"));
+
+        //Func<Osoba, bool> uvjet = (Osoba osoba) => { return osoba.Sifra > 10; };
+        Func<Osoba, bool> uvjet = _ => _.Sifra > 10;
+
+        osobe.Where(osoba => osoba.Sifra > 10);
+        osobe.Where(uvjet);
+
+        void odavdeDoBeskraja(int i)
+        {
+            odavdeDoBeskraja(i);
+            Console.WriteLine($"Prolaz {i}. puta");
+            i++;
+        }
+        odavdeDoBeskraja(1);
+
+        #endregion
     }
-};
 
-Console.WriteLine($"Osoba, Ime: {osoba.Ime}, Račun, Naziv: {osoba.Racun.Naziv}");
+    private static void IzlistajOsobu(Osoba clan)
+    {
+        Console.WriteLine($"Hello world from {clan.Naziv}!");
+    }
 
-var polaznici = new[]
+    private static void ObaviRadnju(HelloWorldDelegate delegat)
+    {
+        delegat.Invoke("Maja");
+    }
+}
+
+public class Osoba
 {
-    new { Id = 10, Name = "Pero" },
-    new { Id = 20, Name = "Anja" },
-    new { Id = 30, Name = "Maja" },
-    new { Id = 40, Name = "Marko" }
-};
+    public int Sifra { get; set; }
 
-var zadnji = polaznici.Last();
-var pretraga = polaznici.Where(_ => _.Id > 10).ToList();
+    public string Naziv { get; set; }
 
-pretraga.ForEach(x => Console.WriteLine($"Polaznik: šifra: {x.Id}, ime: {x.Name}"));
-    
+    public Osoba(int sifra, string naziv)
+    {
+        Sifra = sifra;
+        Naziv = naziv;
+    }
+}
 
 
 
