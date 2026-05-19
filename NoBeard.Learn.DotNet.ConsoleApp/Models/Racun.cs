8972bfe6bac96e5d270bc08cb70b21f5091022b4
Ratko Ćosić <ratko_cosic@hotmail.com>
2026-05-19 20:04:26 +0200

    init

---
+++ NoBeard.Learn.DotNet.ConsoleApp/Models/Racun.cs
@@ -0,0 +1,36 @@
+﻿namespace NoBeard.Learn.DotNet.ConsoleApp.Models;
+
+[Serializable]
+public class Racun
+{
+    public int Sifra { get; set; }
+
+    public string Naziv { get; set; }
+
+    public double Stanje { get; set; }
+
+    public Racun()
+    {
+    }
+
+    //public void Uplata(double iznos)
+    //{
+    //    if (iznos > 0)
+    //    {
+    //        Stanje += iznos;
+    //    }
+    //}
+
+    //public void Isplata(double iznos)
+    //{
+    //    if (iznos > 0)
+    //    {
+    //        Stanje -= iznos;
+    //    }
+    //}
+
+    public override string ToString()
+    {
+        return $"Racun, sifra: {Sifra}, naziv: {Naziv}";
+    }
+}
--- NoBeard.Learn.DotNet.ConsoleApp/Program.cs
+++ NoBeard.Learn.DotNet.ConsoleApp/Program.cs
@@ -1,10 +1,52 @@
-﻿namespace NoBeard.Learn.DotNet.ConsoleApp
+﻿using NoBeard.Learn.DotNet.ConsoleApp.Models;
+using System.Text.Json;
+
+// JSON serijalizacija
+
+var racun = new Racun();
+racun.Sifra = 1000;
+racun.Naziv = "Tekući";
+
+SerijalizirajRacun(racun);
+
+var rn1 = DeserijalizirajRacun();
+Console.WriteLine(rn1?.Sifra);
+
+static void SerijalizirajRacun(Racun racun)
 {
-    internal class Program
+    var options = new JsonSerializerOptions()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        WriteIndented = true,
+    };
+
+    try
+    {
+        var json = JsonSerializer.Serialize(racun, options);
+        File.WriteAllText("racun.json", json);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
+
+static Racun? DeserijalizirajRacun()
+{
+    string? json = null;
+
+    try
+    {
+        json = File.ReadAllText("racun.json");
+        return JsonSerializer.Deserialize<Racun>(json);
+    }
+    catch (FileNotFoundException)
+    {
+        Console.WriteLine("Datoteka nije nađena!");
+        return null;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+        return null;
     }
 }
