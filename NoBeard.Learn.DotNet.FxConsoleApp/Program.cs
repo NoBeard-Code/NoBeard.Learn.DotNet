86b04dfc63e8f787048daca5f885260a687a1377
Ratko Ćosić <ratko_cosic@hotmail.com>
2026-06-02 19:12:12 +0200

    servis

--- NoBeard.Learn.DotNet.FxConsoleApp/Program.cs
+++ NoBeard.Learn.DotNet.FxConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using NoBeard.Learn.DotNet.FxConsoleApp.Services;
+using System.Configuration;
 using System.Linq;
 
 namespace NoBeard.Learn.DotNet.FxConsoleApp
@@ -15,7 +16,22 @@ namespace NoBeard.Learn.DotNet.FxConsoleApp
             // DML:
             // UnosZaposlenika(dbContext);
 
-            var upit = dbContext.Zaposleniks.FirstOrDefault();
+            //var pero = dbContext.Zaposleniks.FirstOrDefault();
+
+            //pero.Adresa = "Trg Pere Perića";
+            //pero.Telefon = "231232312";
+
+            //UrediZaposlenika(dbContext, pero);
+
+            var servis = new PodatkovniServis(dbContext);
+
+            var pero = servis.DohvatiZaposlenika(2);
+
+            pero.Email = "pero2@test.me";
+
+            servis.UrediZaposlenika(pero);
+
+            servis.ObrisiZaposlenika(pero);
         }
 
         private static void UnosZaposlenika(DataClassesDataContext dbContext)
@@ -29,10 +45,17 @@ namespace NoBeard.Learn.DotNet.FxConsoleApp
                 OdjelID = 1
             };
 
+            // INSERT INTO Zaposlenik VALUES ...
             dbContext.Zaposleniks.InsertOnSubmit(zaposlenik);
 
             dbContext.SubmitChanges();
+        }
 
+        private static void UrediZaposlenika(DataClassesDataContext dbContext, Zaposlenik zaposlenik)
+        {
+            // UPDATE Zaposlenik SET ... WHERE ...
+
+            dbContext.SubmitChanges();
         }
     }
 }
---
+++ NoBeard.Learn.DotNet.FxConsoleApp/Services/PodatkovniServis.cs
@@ -0,0 +1,70 @@
+﻿using System.Linq;
+
+namespace NoBeard.Learn.DotNet.FxConsoleApp.Services
+{
+    internal class PodatkovniServis
+    {
+        private readonly DataClassesDataContext _dbContext;
+
+        public PodatkovniServis(DataClassesDataContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Zaposlenik DohvatiZaposlenika(int id)
+        {
+            return _dbContext.Zaposleniks.SingleOrDefault(_ => _.ID == id);
+        }
+
+        public Zaposlenik DohvatiZaposlenika(string imePrezime)
+        {
+            return _dbContext.Zaposleniks
+                .Where(_ => _.ImePrezime.Contains(imePrezime))
+                .FirstOrDefault();
+        }
+
+        public void UnosZaposlenika(Zaposlenik zaposlenik)
+        {
+            // INSERT INTO Zaposlenik VALUES ...
+            _dbContext.Zaposleniks.InsertOnSubmit(zaposlenik);
+
+            _dbContext.SubmitChanges();
+        }
+
+        public void UrediZaposlenika(Zaposlenik zaposlenik)
+        {
+            // UPDATE Zaposlenik SET ... WHERE ...
+
+            _dbContext.SubmitChanges();
+        }
+
+        public void UrediZaposlenika(int id, string adresa, string telefon)
+        {
+            var zaposlenik = DohvatiZaposlenika(id);
+
+            // promijeniti svojstva
+            zaposlenik.Adresa = adresa;
+            zaposlenik.Telefon = telefon;
+
+            _dbContext.SubmitChanges();
+        }
+
+        public void ObrisiZaposlenika(int id)
+        {
+            // DELETE FROM Zaposlenik WHERE Id = ...
+
+            var zaposlenik = DohvatiZaposlenika(id);
+
+            _dbContext.Zaposleniks.DeleteOnSubmit(zaposlenik);
+            _dbContext.SubmitChanges();
+        }
+
+        public void ObrisiZaposlenika(Zaposlenik zaposlenik)
+        {
+            // DELETE FROM Zaposlenik WHERE Id = ...
+
+            _dbContext.Zaposleniks.DeleteOnSubmit(zaposlenik);
+            _dbContext.SubmitChanges();
+        }
+    }
+}
